8593c6b537208715024a7d660c06f50108805805
hlumelontwanambi <st10383786@vcconnect.edu.za>
2024-10-24 13:13:43 +0200

    Add project files.

---
+++ Model/DbApiContext.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFAPIMarvel1.Model;
+
+public partial class DbApiContext : DbContext
+{
+    public DbApiContext()
+    {
+    }
+
+    public DbApiContext(DbContextOptions<DbApiContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TblAvenger> TblAvengers { get; set; }
+
+    public virtual DbSet<TblContact> TblContacts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("data source=labVMH8OX\\SQLEXPRESS;initial catalog=dbAPI;Integrated Security=true; Encrypt=False;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TblAvenger>(entity =>
+        {
+            entity.HasKey(e => e.Username).HasName("PK__tblAveng__536C85E53A4055DA");
+
+            entity.ToTable("tblAvengers");
+
+            entity.Property(e => e.Username)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Password)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<TblContact>(entity =>
+        {
+            entity.HasKey(e => e.AvengerId).HasName("PK__tblConta__3E460B6AA406848A");
+
+            entity.ToTable("tblContacts");
+
+            entity.Property(e => e.AvengerId).HasColumnName("AvengerID");
+            entity.Property(e => e.EmailAddress)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.HeroName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.PhoneNumber)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+            entity.Property(e => e.RealName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Username)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.UsernameNavigation).WithMany(p => p.TblContacts)
+                .HasForeignKey(d => d.Username)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__tblContac__Usern__4CA06362");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Program.cs
@@ -0,0 +1,116 @@
+
+using EFAPIMarvel1.Model;
+
+namespace EFAPIMarvel1
+{
+    public class Program
+    {
+        static DbApiContext db = new DbApiContext();
+
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+            //GET endPOINTS
+            app.MapGet("/users", () => db.TblAvengers.ToList())
+            .WithName("/GetUsers")
+            .WithOpenApi();
+
+            app.MapGet("/contacts", () => db.TblContacts.ToList())
+            .WithName("/GetContacts")
+            .WithOpenApi();
+
+            //POST endPoints
+            app.MapPost("/users", (TblAvenger newUser) =>
+            {
+                db.TblAvengers.Add(newUser);
+                db.SaveChanges();
+                return Results.Created($"/users/{newUser.Username}", newUser);
+            }).WithName("CreateUser").WithOpenApi();
+
+            app.MapPost("/contacts", (TblContact newContact) =>
+            {
+                db.TblContacts.Add(newContact);
+                db.SaveChanges();
+                return Results.Created($"/users/{newContact.Username}", newContact);
+            }).WithName("CreateContract").WithOpenApi();
+
+
+            //PUT endpoints
+            app.MapPut("/users/{username}", (string username, TblAvenger updatedUser) =>
+            {
+                var user = db.TblAvengers.FirstOrDefault(u => u.Username == username);
+                if (user != null)
+                {
+                    user.Password = updatedUser.Password;
+                    db.SaveChanges();
+                    return Results.NoContent();
+                }
+                return Results.NotFound();
+
+            }).WithName("UpdateUser").WithOpenApi();
+
+            app.MapPut("/contacts/{id}", (int id, TblContact updatedContact) =>
+            {
+                var contact = db.TblContacts.FirstOrDefault(c => c.AvengerId == id);
+                if (contact != null)
+                {
+                    contact.HeroName = updatedContact.HeroName;
+                    contact.RealName = updatedContact.RealName;
+                    db.SaveChanges();
+                    return Results.NoContent();
+                }
+                return Results.NotFound();
+
+            }).WithName("UpdateContact").WithOpenApi();
+
+            //DELETE endpoints
+            app.MapDelete("/users/{username}", (string username) =>
+            {
+                var user = db.TblAvengers.FirstOrDefault(u => u.Username == username);
+                if (user != null)
+                {
+                    db.TblAvengers.Remove(user);
+                    db.SaveChanges();
+                    return Results.NoContent();
+                }
+                return Results.NotFound();
+
+            }).WithName("DeleteUser").WithOpenApi();
+
+            app.MapDelete("/contacts/{id}", (int id) =>
+            {
+                var contact = db.TblContacts.FirstOrDefault(c => c.AvengerId == id);
+                if (contact != null)
+                {
+                    db.TblContacts.Remove(contact);
+                    db.SaveChanges();
+                    return Results.NoContent();
+                }
+                return Results.NotFound();
+
+            }).WithName("DeleteContact").WithOpenApi();
+
+            app.Run();
+        }
+    }
+}
